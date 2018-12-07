a7bfbbd3f986b0413e1d2793024c0f534493c162
meesf <m.froberg@students.uu.nl>
2018-12-07 10:48:00 +0100

    standard deviations of ink

--- main.R
+++ main.R
@@ -3,19 +3,16 @@
 
 library(MASS)
 
-# Read data ---------------------------------------------------------------
-train.dat <- read.csv(file="optdigits-tra.txt", sep=",")
-test.dat <- read.csv(file="optdigits-tes.txt", sep=",")
 
-summary(trainData)
-summary(testData)
-# The corner pixels often are 0 (mean is very low), therefore they dont really influence the classification process.
+# Read data ---------------------------------------------------------------
 
 mnist <- read.csv(file="mnist.csv")
 mnist[,1] <- as.factor(mnist[,1])
 summary(mnist[,1])
 
 # findings:
+# 
+# The corner pixels often are 0 (mean is very low), therefore they dont really influence the classification process.
 # 
 # > summary(myData[,1])
 # 0    1    2    3    4    5    6    7    8    9 
@@ -24,8 +21,34 @@ summary(mnist[,1])
 # the dataset contains 42000 samples in total. Pictures with label "1" occur the most and those with label "5" 
 # the least. there is a substantial difference between these labels 4684 - 3795 = 889....
 
-# Analyse data ------------------------------------------------------------
+# Ink ---------------------------------------------------------------------
 
 # Get ink amount per datapoint
-ink <- apply(mnist, 1, function(x) sum(x[-1]))
-inkdf <- data.frame(label=mnist[,1],ink=ink)
+ink <- apply(mnist[,2:785], 1, function(x) sum(x))
+mean <- tapply(ink, mnist[,1], mean)
+sd <- tapply(ink, mnist[,1], sd)
+mean  # prints the mean values
+sd    # prints the standard deviations
+
+# Using this feature to classify the pixel data will not be able to differentiate between some of the digits. Mainly because the mean values of most of 
+# the classes are very close to each other. Besides the rather close means the standard deviations are also very high for most digits, therefore their 
+# normal distributions are overlapping each other for large parts. Subsequently this feature will not be able to distinguish between these digits.
+
+# Plot normal distributions
+colors <- c("red", "blue", "darkgreen", "gold", "black", "yellow", "green", "purple", "pink", "orange")
+labels <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+x <- seq(5000, 60000, length=100000)
+plot(x, dnorm(x, mean=mean[1], sd=sd[1]), type="l", col=colors[1], xlab="Ink", ylab="Density", ylim=c(0,0.0001))
+for(i in 2:10) {
+  lines(x, dnorm(x,mean=mean[i], sd=sd[i]), col=colors[i])
+}  
+legend("topright", inset=.05, title="Digits",legend=labels, col=colors,lty=c(1,1,1,1,1,1,1,1,1,1))
+
+# Using these normal distributions the classes can roughly be divided into 4 groups. These are digits:
+# - 1
+# - 4, 7 and 9
+# - 2,3,5,6 and 8
+# - 0
+
+# Train multinomial model
+inkScaled <- scale(ink)
