56c4243a41d52b654a14080b48a79e5cf4b7fc7c
meesf <m.froberg@students.uu.nl>
2018-12-10 21:27:24 +0100

    work in progress

--- main.R
+++ main.R
@@ -47,7 +47,7 @@ normalDistPlot <- function(features, seqA, seqB, lim) {
   legend("topright", inset=.05, title="Digits",legend=labels, col=colors,lty=c(1,1,1,1,1,1,1,1,1,1))
 }
 
-# Ink ---------------------------------------------------------------------
+# Ink feature ---------------------------------------------------------------------
 
 # Get ink amount per datapoint
 ink <- apply(mnist[,2:785], 1, function(z) sum(z))
@@ -81,20 +81,19 @@ test(features) # You have to load the test function first (see "All features" re
 
 
 # Ink per row ---------------------------------------------------------------------
-# New feature: average the ink per row of the greyscale images, only take rows into account that contain ink.
+# New feature: sum the ink per row of the greyscale images.
 # This feature will be able to discriminate between digits that differ in their horizontal shape. For example ones will have very different 
 # patterns compared to eights, because on average 1 has a narrow shape and 8 is more broad.
-averageRowInk <- function(x) {
+rowInk <- function(x) {
   m <- matrix(x, nrow=28)
   result <- c()
   for(i in 1:28){
-    if(sum(m[i,]) > 0) {
-      result <- append(result, as.numeric(sum(m[i,])))
-    }
+    result <- append(result, as.numeric(sum(m[i,])))
   }
-  return(mean(result))
+  return(result)
 }
 
+# This feature counts the number of rows that have ink
 rowsWithInk <- function(x) {
   m <- matrix(x, nrow=28)
   result <- 0
@@ -106,7 +105,7 @@ rowsWithInk <- function(x) {
   return(result)
 }
 
-width <- apply(mnist[,2:785], 1, function(z) averageRowInk(z))
+width <- t(apply(mnist[,2:785], 1, function(z) rowInk(z)))
 features <- data.frame("label"=y, "width"=width)
 normalDistPlot(features, 0, 4000, 0.002)
 
@@ -116,15 +115,13 @@ normalDistPlot(features, 0, 28, 0.2)
 
 # Ink per col ---------------------------------------------------------------------
 # New feature: This is the same as the ink per row feature, only this one is for columns.
-averageColInk <- function(x) {
+colInk <- function(x) {
   m <- matrix(x, nrow=28)
   result <- c()
   for(i in 1:28){
-    if(sum(m[,i]) > 0) {
-      result <- append(result, as.numeric(sum(m[,i])))
-    }
+    result <- append(result, as.numeric(sum(m[,i])))
   }
-  return(mean(result))
+  return(result)
 }
 
 colsWithInk <- function(x) {
@@ -138,7 +135,7 @@ colsWithInk <- function(x) {
   return(result)
 }
 
-height <- apply(mnist[,2:785], 1, function(z) averageColInk(z))
+height <- t(apply(mnist[,2:785], 1, function(z) colInk(z)))
 features <- data.frame("label"=y, "width"=height)
 normalDistPlot(features, 0, 4000, 0.002)
 
@@ -146,16 +143,46 @@ numCols  <- apply(mnist[,2:785], 1, function(z) colsWithInk(z))
 features <- data.frame("label"=y, "width"=numCols)
 normalDistPlot(features, 0, 28, 1.2)
 
-# All features ---------------------------------------------------------------------
-# Here we can analyse all our features. First load the features that you want to test as a data.frame, then pass that to the test function.
-# getFoldIndices <- function(y, k = 10) {
-#   require(caret)
-#   return(createFolds(y, k = k, list = TRUE, returnTrain = FALSE))
-# }
-
-getLambda <- function(x, y, alpha = 1, k = 5) {
-  return(cv.glmnet(x, y, alpha = alpha, family = "multinomial", nfolds=k, type.measure = "class", grouped = FALSE))
+# Reduce size ---------------------------------------------------------------------
+# Here we introduce a function that reduces the size of the images. from 28x28 --> 14x14 greyscale images.
+# This is achieved by looping over every 2x2 block within the 28x28 image and take the average of that block.
+halfSize <- function(x) {
+  m <- matrix(x, nrow=28)
+  result <- c()
+  for(i in 0:195) {
+    row <- floor(i/14)*2 + 1
+    col <- (i %% 14) * 2 + 1
+    temp <- c(m[row, col],
+              m[row + 1, col],
+              m[row, col + 1],
+              m[row + 1, col + 1])
+    result <- append(result, mean(temp))
+  }
+  return(result)
 }
+
+reducedImages <- t(apply(mnist[,2:785], 1, function(x) halfSize(x)))
+
+# Remove superfluous ---------------------------------------------------------------------
+removeSuperfluous <- function(data, npixels) {
+  indices <- c()
+  for(i in 1:npixels) {
+    superfluous <- TRUE
+    for(j in 1:nrow(data)) {
+      if(data[j, i] > 0) superfluous <- FALSE
+    }
+    if(! superfluous) {
+      indices <- append(indices, i)
+    }
+  }
+  return(data[,indices])
+}
+
+
+reducedImages <- removeSuperfluous(reducedImages, 196)
+
+# Classify ---------------------------------------------------------------------
+# Here we can analyse all our features. First load the features that you want to test as a data.frame, then pass that to the test function.
 
 test <- function(features, trainSize = 0.2, folds = 5, k = 5) {
   # Prepare data
@@ -167,13 +194,14 @@ test <- function(features, trainSize = 0.2, folds = 5, k = 5) {
   train.y <- train.data$label
   test.x <- model.matrix(label ~., test.data)[,-1]
   test.y <- test.data$label
+  print("Prepared  the data")
   
   # Train multinomial logit model
   cv.fit <- getLambda(train.x, train.y, k = folds)
   multinom.model <- glmnet(train.x, train.y, alpha = 1, family="multinomial")
   # Test the model
   multinom.pred <- predict(multinom.model, test.x, s=cv.fit$lambda.min, type = "class")
-  
+  print("trained multinom model")
   
   # Predict using knn
   knn.pred <- knn(train.x, test.x, train.y, k = k)
@@ -184,11 +212,32 @@ test <- function(features, trainSize = 0.2, folds = 5, k = 5) {
   print(paste("knn:", mean(knn.pred == test.y)))
 }
 
+getLambda <- function(x, y, alpha = 1, k = 5) {
+  return(cv.glmnet(x, y, alpha = alpha, family = "multinomial", nfolds=k, type.measure = "class", grouped = FALSE))
+}
+
 
 allFeatures <- data.frame("label"=y, "ink"=scale(ink), "width"=scale(width), "height"=scale(height), "numCols"=scale(numCols), "numRows"= scale(numRows))
-
-
 features <- data.frame("label"=y, "ink"=scale(ink), "width"=scale(width))
-test(features, k = 3)
+features <- data.frame("label"=y, reducedImages)
+
+features <- data.frame("label"=y, width, height)
+
+test(features, trainSize = 0.1, k = 3)
+
+
+
+# 1. What data pre-processing (including feature extraction/feature selection) did you perform for this classification algorithm? New 
+#    features that you derive from the raw pixel data must be described unambiguously in the report. The reader should be able to 
+#    reproduce your analysis. 
+# First describe the extracted features (these are ink, averageRowInk, rowsWithInk, averageColInk, colsWithInk) TODO: add more features
+# For every classification algorithm the data is split into train set and a test set. The trainset is a list of randomly selected samples 
+# from the data. The size is equal to 10% of the total datapoints (this is 4200). 
+# - Multinomial logit: a lambda value for the regularization has to be calculated. 
+# - Knn: 
+# - SVM: 
+
+# 2. What are the complexity parameters (if any) of the classification algorithm, and how did you select their values?
+# 3. What is the estimated accuracy of the best classifier for this method? 
 
 
